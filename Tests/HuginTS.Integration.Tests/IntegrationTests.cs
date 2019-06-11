7aa8508a045f939399aff98ebbb5eceb242d05a6
Mats Andersson <mats@greyhouse.se>
2019-06-11 08:42:34 +0200

    Adding unittests

--- Tests/HuginTS.Integration.Tests/IntegrationTests.cs
+++ Tests/HuginTS.Integration.Tests/IntegrationTests.cs
@@ -33,5 +33,34 @@ namespace HuginTS.Integration.Tests
             dataPoints.First().Timestamp.ShouldBe(timeStamp);
             dataPoints.First().Value.ShouldBe(20);
         }
+
+        [Fact]
+        public async void Should_Store_Two_DataPoints_On_Different_Partitions()
+        {
+            var runner = MongoDbRunner.Start();
+            var client = new MongoClient(runner.ConnectionString);
+
+            // Arrange
+            var dataPointService = new DataPointService(new TimeseriesPartitionFactory(client.GetDatabase("test")));
+
+            var timeStamp1 = new DateTime(2019, 1, 1, 1, 0, 0);
+            var timeStamp2 = new DateTime(2019, 1, 2, 1, 0, 0);
+
+            await dataPointService.RegisterAsync("series1", timeStamp1, 20);
+            await dataPointService.RegisterAsync("series1", timeStamp2, 30);
+
+            // Act
+
+            var dataPoints = await dataPointService.GetByPeriodAsync("series1", new DateTime(2019, 1, 1, 0, 0, 0),
+                new DateTime(2019, 1, 20, 3, 0, 0));
+
+            // Assert
+            dataPoints.Count.ShouldBe(2);
+            dataPoints[0].Timestamp.ShouldBe(timeStamp1);
+            dataPoints[0].Value.ShouldBe(20);
+            dataPoints[1].Timestamp.ShouldBe(timeStamp2);
+            dataPoints[1].Value.ShouldBe(30);
+
+        }
     }
 }
